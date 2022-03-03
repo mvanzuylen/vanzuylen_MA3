9bec138d44aec6a7e4e0ee921fa0d080dd714478
mvanzuylen <madeleine.vanzuylen@gmail.com>
2022-03-02 20:46:42 -0800

    EC done

---
+++ madeleine_vanzuylen-mni-assignment-3/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css'
+
+export default function App(){
+    return(
+    <body>
+        <div class="grid-container ">
+        <div class="item1 grid-content-format">
+            <h1>Madeleine van Zuylen</h1>
+        </div>
+        <div class="item2 grid-content-format">
+        </div>
+        <div class="item3 grid-content-format">
+            <div class="box"></div>
+        </div>  
+        <div class="item4 grid-content-format"></div>
+        <div class="item5 grid-content-format"></div>
+      </div>
+    </body>  
+    )
+}
